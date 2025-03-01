20da6fc4039f8195d242d09cde5f55770fef04b8
SarathSobhitharaj <sarathsobhitharaj@gmail.com>
2025-03-01 14:43:21 +0530

    udated webelement

--- src/main/java/org/example/OurDailyBread/BreadWebElements.java
+++ src/main/java/org/example/OurDailyBread/BreadWebElements.java
@@ -1,5 +1,9 @@
 package org.example.OurDailyBread;
 
 public class BreadWebElements {
+    public static String Emails = "//div[@class='control _with-tooltip']/input[@name='username']";
+    public static String firstnames = "//div/input[@name='firstname']";
+    public static String lastnames = "//div/input[@name='lastname']";
+    public static String companys = "//div/input[@name='company']";
 
 }
